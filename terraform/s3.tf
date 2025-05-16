9fc9cfe07843c184c33acb2a61f5da6797208073
Antoine Delia <antoine.delia@gmail.com>
2025-05-16 11:39:02 +0200

    feat: redirect to landing page

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,13 +5,8 @@ resource "aws_s3_bucket" "site" {
 
 resource "aws_s3_bucket_website_configuration" "site" {
   bucket = aws_s3_bucket.site.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
+  redirect_all_requests_to {
+    host_name = "linktr.ee/antoinedelia"
   }
 }
 
